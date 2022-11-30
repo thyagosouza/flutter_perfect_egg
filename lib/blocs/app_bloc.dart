84387a230f1a5f32775cf4763e3549134ddce23a
thyagosouza <thyagosouza@gmail.com>
2022-11-29 23:19:16 -0300

    03 -  Configurando o Provider

---
+++ lib/blocs/app_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppBloc extends ChangeNotifier {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'blocs/app_bloc.dart';
 import 'pages/splash_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppBloc>.value(
+          value: AppBloc(),
+        ),
+      ],
+      child: Main(),
+    );
+  }
+}
 
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
