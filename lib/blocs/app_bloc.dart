be0970443b0aa3e7f0a23a982453b877b3e07133
thyagosouza <thyagosouza@gmail.com>
2022-11-29 23:43:46 -0300

    04 -  Criando o BLOC

--- lib/blocs/app_bloc.dart
+++ lib/blocs/app_bloc.dart
@@ -1,8 +1,79 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class AppBloc extends ChangeNotifier {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  late Timer timer;
+  String state = 'stopped';
+  String selected = 'soft';
+  double time = 0.1 * 60.0;
+  double seconds = 0;
+  double percent = 0;
+
+  void updateTime() {
+    seconds++;
+    percent = (seconds * 100) / time;
+    notifyListeners();
+    if (seconds == time) done();
+  }
+
+  select(String type) {
+    switch (type) {
+      case 'soft':
+        {
+          selected = 'soft';
+          time = 5.0 * 60.0;
+          notifyListeners();
+          return;
+        }
+        break;
+      case 'medium':
+        {
+          selected = 'medium';
+          time = 7.0 * 60.0;
+          notifyListeners();
+          return;
+        }
+        break;
+      case 'hard':
+        {
+          selected = 'hard';
+          time = 10.0 * 60.0;
+          notifyListeners();
+          return;
+        }
+      default:
+        {
+          time = 5.0 * 60.0;
+          notifyListeners();
+          return;
+        }
+    }
+  }
+
+  void start() {
+    state = 'cooking';
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => updateTime());
+    notifyListeners();
+  }
+
+  void stop() {
+    timer.cancel();
+    state = 'stopped';
+    seconds = 0;
+    percent = 0;
+    notifyListeners();
+  }
+
+  void done() {
+    timer.cancel();
+    state = 'done';
+    seconds = 0;
+    percent = 0;
+    notifyListeners();
+  }
+
+  void reset() {
+    stop();
   }
 }
